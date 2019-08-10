ebc86a7b61879256858c00efa9b48e43896cb406
E.B <edenbenbow@yahoo.com>
2019-08-10 03:20:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
-  for (let i = 0; i < array.length; i++) {
-  if (array[i] !== stopValue) {
+  for(let i = 0; i < array.length; i++) {
+  if(array[i] !== stopValue) {
 array[i] = changeValue[i]
 }
 else{
