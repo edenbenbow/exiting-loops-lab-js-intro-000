baf30bbfd9fba2ecfe404409076177154cd0addf
E.B <edenbenbow@yahoo.com>
2019-08-10 03:40:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ return array;
 
 function keepGoing(array, changeValue, skipValue) {
 for(let i = 0; i < array.length; i++) {
-if(array[i] !== skipValue)
+if(array[i] !== skipValue) {
+  array[i] = changeValue
+}
 return changeValue
 
 else if(array[i] = skipValue)
