47bd9012b860d9efb9e56f339ce4350bd70f91bc
E.B <edenbenbow@yahoo.com>
2019-08-10 03:14:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
+  var array = [];
+  var changeValue = [];
   for (i = 0; i < array.length; i++) {
   if (array[i] !== stopValue) {
 return changeValue
